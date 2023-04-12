6f7870d2c154351d605aef167d994415c52d23cb
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-12 18:47:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
+import scipy.stats as stats
 import pandas as pd
 import numpy as np
 
+chat_id = 704471350
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x: np.array) -> bool:
+    Ttest = stats.ttest_1samp(a=x, popmean=500)
+    alpha = 0.02
+    if alpha < Ttest.pvalue:
+      res = False
+    else:
+      res = True
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return res
